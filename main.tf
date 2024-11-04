cf65d764c196a44b837c35f8dd3a8612e32dfbd5
lenovo <omkar.reddy211@gmailcom>
2024-11-04 22:43:47 +0530

    payment script

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "ec2" {
-    for_each = var.instance
-    source = "./modules"
-    env = var.env
-    app_port = each.value["app_port"]
+    for_each   = var.instance
+    source     = "./modules"
+    env        = var.env
+    app_port   = each.value["app_port"]
     component_name = each.key
     instance_name = each.value["instance_name"]
     }
